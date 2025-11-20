db08f76c2fa0cd9286cb0182a934c09ff4f12e8e
이기봉 <whitebong@igibong-ui-MacBookAir.local>
2025-11-20 23:32:16 +0900

    feat: 로그아웃 처리 로그 & /me 호출시 401 에러 반환시 로그인 페이지로 리다이렉트

--- hooks/useLogout.ts
+++ hooks/useLogout.ts
@@ -7,18 +7,33 @@ import { useAuthStore } from "@/stores/useAuthStore";
 
 export const useLogout = () => {
   const router = useRouter();
+  const addLog = useAuthStore((state) => state.addLog);
   const clearUser = useAuthStore((s) => s.clearUser);
 
   return useMutation({
     mutationFn: logout,
 
-    onSuccess: () => {
+    onSuccess: (res) => {
+      addLog({
+        status: "success",
+        data: res,
+        timestamp: new Date().toISOString(),
+      });
       clearUser();
       // 쿠키 제거는 서버에서 하고 있으므로 클라이언트는 이동만
       router.push("/signin");
     },
 
     onError: (err: any) => {
+      const backendError = err?.response?.data || null; // ✅ 404 JSON도 포함됨
+      const statusCode = err?.response?.status || "Unknown";
+
+      addLog({
+        status: "error",
+        error: backendError || { message: err.message, statusCode },
+        timestamp: new Date().toISOString(),
+      });
+
       clearUser();
       console.error("Logout error:", err);
       router.push("/signin");
--- hooks/useMe.ts
+++ hooks/useMe.ts
@@ -4,6 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 import { me } from "@/api/partner-api";
 import { useAuthStore } from "@/stores/useAuthStore";
 import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 interface MeResponse {
   success: boolean;
@@ -16,6 +17,7 @@ export const useMe = () => {
   const addLog = useAuthStore((state) => state.addLog);
   const setUser = useAuthStore((state) => state.setUser);
   const clearUser = useAuthStore((state) => state.clearUser);
+  const router = useRouter();
 
   const query = useQuery<MeResponse>({
     queryKey: ["me"],
@@ -50,7 +52,17 @@ export const useMe = () => {
         timestamp: new Date().toISOString(),
       });
 
-      clearUser();
+      setTimeout(() => {
+        if (statusCode === 401) {
+          addLog({
+            status: "error",
+            error: `로그인 페이지로 리다이렉트`,
+            timestamp: new Date().toISOString(),
+          });
+          clearUser();
+          router.push("/signin");
+        }
+      }, 2000);
     }
   }, [query.isError]);
 
