3e542cb383cd989d6837e54a86441766c0d6f452
이기봉 <whitebong@igibong-ui-MacBookAir.local>
2026-01-22 16:04:15 +0900

    fix: 포트 컨벤션에 맞게 수정

--- api/generated/core/OpenAPI.ts
+++ api/generated/core/OpenAPI.ts
@@ -20,7 +20,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-  BASE: "http://localhost:3000/api",
+  BASE: "http://localhost:4110/api",
   VERSION: "1.0.0",
   WITH_CREDENTIALS: true,
   CREDENTIALS: "include",
--- api/partner-api.ts
+++ api/partner-api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const partnerApi = axios.create({
-  baseURL: "http://localhost:3000/api", // 파트너 백앤드
+  baseURL: "http://localhost:4110/api", // 파트너 백앤드
   headers: {
     "Content-Type": "application/json",
   },
